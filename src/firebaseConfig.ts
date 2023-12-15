f574283cab751c35f5e49db373213bb0b1f78128
benwin22 <145385526+benwin22@users.noreply.github.com>
2023-12-15 09:06:17 -0600

    morning of pres

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,4 +1,22 @@
-export const firebaseConfig = {
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyBM26NyEXf7HMJELyZpHu8iQxHz1Hhrm4g",
+//   authDomain: "animal-search-ct-capstone.firebaseapp.com",
+//   databaseURL: "https://animal-search-ct-capstone-default-rtdb.firebaseio.com",
+//   projectId: "animal-search-ct-capstone",
+//   storageBucket: "animal-search-ct-capstone.appspot.com",
+//   messagingSenderId: "583310555313",
+//   appId: "1:638405879158:web:aa523c2b3f2511c248c657"
+// };
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDKqdG3pnNrsd-DG4Vs5YOR5ODpujebz2Y",
+//   authDomain: "dino-data-capstone.firebaseapp.com",
+//   databaseURL: "https://dino-data-capstone-default-rtdb.firebaseio.com",
+//   projectId: "dino-data-capstone",
+//   storageBucket: "dino-data-capstone.appspot.com",
+//   messagingSenderId: "638405879158",
+//   appId: "1:638405879158:web:aa523c2b3f2511c248c657"
+// };
+ export const firebaseConfig = {
   apiKey: "AIzaSyBM26NyEXf7HMJELyZpHu8iQxHz1Hhrm4g",
   authDomain: "animal-search-ct-capstone.firebaseapp.com",
   databaseURL: "https://animal-search-ct-capstone-default-rtdb.firebaseio.com",
